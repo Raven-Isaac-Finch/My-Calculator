58e5826ee6bef7a11871d8c67c15956d3cc75e07
Raven <purplebox02@gmail.com>
2022-05-23 13:26:44 +0300

    Project Completed & Bug Fixes

--- main.js
+++ main.js
@@ -1,49 +1,80 @@
-let currentNumber = document.querySelector('.current-number');
-let previousNumber = document.querySelector('.previous-number');
+let currentNumber = document.querySelector('.current');
+let previousNumber = document.querySelector('.previous');
 let result = 0;
+let resetToken = 'oldRegister';
+let operator = '';
 
 const calculator = document.querySelector('.calculator');
 
 
 calculator.addEventListener('click', function(e) {
     if(e.target.classList.contains("operation")) {
-        operationCheck(e.target.textContent);
+        if(operator == '') {
+            if(isNaN(parseInt(currentNumber.textContent))) return;
+            operationCheck(e.target.textContent);
+        } else if(operator !== '') {
+            if(isNaN(parseInt(currentNumber.textContent))) return;
+            compute(operator);
+            previousNumber.textContent = `${currentNumber.textContent} ${e.target.textContent}`;
+            currentNumber.textContent = '';
+            resetToken = 'oldRegister';
+        }
+    operator = e.target.textContent;
     } else if(e.target.classList.contains("equals")) {
-        compute();
+        compute(operation);
     } else if(e.target.classList.contains("all-clear")) {
         allClear();
     } else if(e.target.classList.contains("delete")) {
         del();
     } else if(e.target.classList.contains("square")) {
         square();
+    } else if (e.target.classList.contains("number")) {
+        if(resetToken == 'newRegister') {
+            previousNumber.textContent = '';
+            currentNumber.textContent = '';
+            currentNumber.textContent += e.target.textContent;
+            resetToken = 'oldRegister';
+        } else if(resetToken == 'oldRegister') {
+            if (e.target.textContent == '.' && currentNumber.textContent.includes('.')) return;
+            currentNumber.textContent += e.target.textContent;
+        }
     } else {
-        currentNumber.textContent += e.target.textContent;
-    }
+        return
+    };
 });
 
 function operationCheck(operation) {
-    compute();
-    previousNumber.textContent = currentNumber.textContent;
+    if(currentNumber.textContent === '') return;
+    if(previousNumber.textContent !== '') {
+        compute(operation);
+    }
+    previousNumber.textContent = `${currentNumber.textContent} ${operation}`;
     currentNumber.textContent = '';
+    this.operation = operation;
+    resetToken = 'oldRegister';
 };
 
-function compute() {
+function compute(operation) {
+    if(isNaN(parseInt(previousNumber.textContent)) || isNaN(parseInt(currentNumber.textContent))) return;
     switch(operation) {
         case "/":
-            result = parseInt(previousNumber.textContent) / parseInt(currentNumber.textContent);
+            result = parseFloat(previousNumber.textContent) / parseFloat(currentNumber.textContent);
             break;
         case "*":
-            result = parseInt(previousNumber.textContent) * parseInt(currentNumber.textContent);
+            result = parseFloat(previousNumber.textContent) * parseFloat(currentNumber.textContent);
             break;
         case "-":
-            result = parseInt(previousNumber.textContent) - parseInt(currentNumber.textContent);
+            result = parseFloat(previousNumber.textContent) - parseFloat(currentNumber.textContent);
             break;
         case "+":
-            result = parseInt(previousNumber.textContent) + parseInt(currentNumber.textContent);
+            result = parseFloat(previousNumber.textContent) + parseFloat(currentNumber.textContent);
             break;
+        default:
+            return
     };
-    currentNumber = result;
-    previousNumber = '';
+    currentNumber.textContent = result;
+    previousNumber.textContent = '';
+    resetToken = 'newRegister';
 };
 
 function allClear() {
@@ -57,6 +88,5 @@ function del() {
 
 function square() {
     result = currentNumber.textContent * currentNumber.textContent;
-
     currentNumber.textContent = result;
 };
